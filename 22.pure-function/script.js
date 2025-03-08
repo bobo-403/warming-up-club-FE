a6c21060ea8884f0a7f6b4e476bb805ef04342df
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2025-03-08 15:31:26 +0900

    pure function

--- 22.pure-function/script.js
+++ 22.pure-function/script.js
@@ -6,5 +6,21 @@
 
 let x = 0;
 const pureNumberUp = (num) => num + 1;
-console.log(pureNumberUp());
+console.log(pureNumberUp(x));
 console.log(x);
+
+const alphabetArray = ['A', 'B'];
+// const addItemToArray = (originalArray, newItem) => {
+//   originalArray.push(newItem);
+//   return originalArray;
+// };
+
+// console.log(addItemToArray(alphabetArray, 'c'));
+// console.log(alphabetArray);
+
+const pureAddItemToArray = (originalArray, newItem) => {
+  return [...originalArray, newItem];
+};
+
+console.log(pureAddItemToArray(alphabetArray, 'c'));
+console.log(alphabetArray);
