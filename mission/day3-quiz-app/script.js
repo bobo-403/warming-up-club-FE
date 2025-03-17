893da92e0246e8b644be95a2f358e4b47d2c594b
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2025-03-17 23:31:53 +0900

    feat: json 문제 데이터 블러오는 기능 추가

---
+++ mission/day3-quiz-app/script.js
@@ -0,0 +1,26 @@
+// 퀴즈 data 불러오기
+async function getData() {
+  const response = await fetch('./data.json');
+  return response.json();
+}
+
+// 선지 버튼을 선택했을때 이벤트
+//  선택한 것이 정답인 경우
+//    선택한 선지를 초록색
+//  선택한 것이 정답이 아닌 경우
+//    선택한 선지 빨간색
+//    정답 선지 초록색
+//  다음 문제가 있을 경우
+//    next 버튼 생성
+//  마지막 문제인 경우
+//    restart 버튼 생성
+
+// next 버튼 선택했을때 이벤트
+//  next 버튼 없애기기
+//  다음 문제 불러오기
+//  선지 버튼 색 원래 상태로
+
+// restart 버튼 선택했을때 이벤트
+//  restart 버튼 삭제
+//  퀴즈 초기 상태로 변환
+//  다시 시작
