8e91046367c95c32931e0368fbe3f9731a229395
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2025-03-17 23:50:56 +0900

    feat: 선지 중 하나를 선택했을 때 결과와 next / restart 버튼 생성하는 기능 추가

--- mission/day3-quiz-app/script.js
+++ mission/day3-quiz-app/script.js
@@ -20,23 +20,6 @@ function createQuiz(data) {
   });
 }
 
-async function main() {
-  const data = await getData();
-
-  const choiceBox = document.querySelector('.buttonWrapper');
-  choiceBox.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'BUTTON') return;
-
-    if (e.target.textContent === 'next') return console.log('next');
-
-    if (e.target.textContent === 'restart') return console.log('restart');
-  });
-
-  createQuiz(data[quizIndex]);
-}
-
-main();
-
 // 선지 버튼을 선택했을때 이벤트
 //  선택한 것이 정답인 경우
 //    선택한 선지를 초록색
@@ -47,6 +30,41 @@ main();
 //    next 버튼 생성
 //  마지막 문제인 경우
 //    restart 버튼 생성
+function viewResult(e, data) {
+  const choices = document.querySelectorAll('.option');
+  const correctIdx = data.choices.indexOf(data.answer);
+  choices[correctIdx].id = 'correct';
+
+  if (e.target.textContent !== data.answer) e.target.id = 'incorrect';
+
+  if (quizIndex < 10) {
+    const next = document.querySelector('.next');
+    next.classList.remove('hidden');
+    return;
+  }
+
+  const restart = document.querySelector('.next');
+  restart.classList.remove('hidden');
+}
+
+async function main() {
+  const data = await getData();
+
+  const choiceBox = document.querySelector('.buttonWrapper');
+  choiceBox.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'BUTTON') return;
+
+    if (e.target.textContent === 'next') return console.log('next');
+
+    if (e.target.textContent === 'restart') return console.log('restart');
+
+    viewResult(e, data[quizIndex]);
+  });
+
+  createQuiz(data[quizIndex]);
+}
+
+main();
 
 // next 버튼 선택했을때 이벤트
 //  next 버튼 없애기기
